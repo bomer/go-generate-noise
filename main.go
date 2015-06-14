86e14bdb55288578824eaa7be2b01634f0c9e259
James O'Toole <james.otoole@industrieit.com>
2015-06-14 20:12:52 +1000

    Trying to implement go routine

--- main.go
+++ main.go
@@ -38,6 +38,41 @@ func ImageRead(ImageFile string) (image image.Image) {
 
 	return img
 }
+func generateImage(w int, h int, imageGenerated int) {
+	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	black := color.RGBA{0, 0, 0, 255}
+	draw.Draw(m, m.Bounds(), &image.Uniform{black}, image.ZP, draw.Src)
+	stringval := fmt.Sprint("output/out", imageGenerated, ".png")
+	fmt.Printf("printing ")
+	fmt.Printf(stringval)
+	fmt.Printf("\n")
+	outpng, err := os.Create(stringval)
+	if err != nil {
+		panic(err.Error())
+	}
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
+			pixel := m.At(x, y).(color.RGBA)
+			// m.Set(x, y, singlePixel)
+			test := rand.Intn(2)
+			if test == 1 {
+				pixel.R = 255
+				pixel.G = 255
+				pixel.B = 255
+			} else {
+				pixel.R = 0
+				pixel.G = 0
+				pixel.B = 0
+			}
+			m.Set(x, y, pixel)
+
+		}
+		// This doesnt work, cannot convert gif image type (paletted), from image.RGBA
+		// outputGif.Image = append(outputGif.Image, ImageRead("out.png"))
+	}
+	png.Encode(outpng, m)
+	outpng.Close()
+}
 
 func main() {
 	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
@@ -50,36 +85,11 @@ func main() {
 	h := m.Bounds().Max.Y
 	fmt.Println("w", w, "h", h)
 	os.Mkdir("output", 0700)
-	numberOfFramesToGenerate := 1000
+	numberOfFramesToGenerate := 20
 	for imageGenerated := 0; imageGenerated < numberOfFramesToGenerate; imageGenerated++ {
 		// fmt.Println(("out" + (imageGenerated) + ".png"))
-		stringval := fmt.Sprint("output/out", imageGenerated, ".png")
-		outpng, err := os.Create(stringval)
-		if err != nil {
-			panic(err.Error())
-		}
-		for x := 0; x < w; x++ {
-			for y := 0; y < h; y++ {
-				pixel := m.At(x, y).(color.RGBA)
-				// m.Set(x, y, singlePixel)
-				test := rand.Intn(2)
-				if test == 1 {
-					pixel.R = 255
-					pixel.G = 255
-					pixel.B = 255
-				} else {
-					pixel.R = 0
-					pixel.G = 0
-					pixel.B = 0
-				}
-				m.Set(x, y, pixel)
+		go generateImage(w, h, imageGenerated)
 
-			}
-			// This doesnt work, cannot convert gif image type (paletted), from image.RGBA
-			// outputGif.Image = append(outputGif.Image, ImageRead("out.png"))
-		}
-		png.Encode(outpng, m)
-		outpng.Close()
 	}
 
 }
